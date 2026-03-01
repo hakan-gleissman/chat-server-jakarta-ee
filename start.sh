09d18bef8087199e8ccd8ad67d89ffbc1200951b
hakan-gleissman <hakan.gleissman@gmail.com>
2026-03-01 23:55:23 +0100

    initial

--- start.sh
+++ start.sh
@@ -4,24 +4,21 @@ set -e
 PORT="${PORT:-8080}"
 echo "Render PORT=${PORT}"
 
-# Starta domain så att asadmin kan ändra config
 asadmin start-domain domain1
 
-# Sätt HTTP-porten till Render-porten
+# Sätt port + bind till alla interfaces
 asadmin set server.network-config.network-listeners.network-listener.http-listener-1.port="${PORT}"
-
-# Viktigt i containers/Render: bind till alla interfaces
 asadmin set server.network-config.network-listeners.network-listener.http-listener-1.address=0.0.0.0
 
-# Rekommenderat: stäng admin-listener (4848)
+# Stäng admin listener (ok om det lyckas, annars fortsätt)
 asadmin set server.network-config.network-listeners.network-listener.admin-listener.enabled=false || true
 
-# Rekommenderat: stäng IIOP (3700) för att slippa GIOP-varningar
-asadmin set configs.config.server-config.iiop-service.enabled=false || true
+# Ta bort eller gör tolerant:
+# asadmin set configs.config.server-config.iiop-service.enabled=false || true
 
-# Logga vad som faktiskt blev satt (bra för Render-loggen)
-asadmin get server.network-config.network-listeners.network-listener.http-listener-1.port
-asadmin get server.network-config.network-listeners.network-listener.http-listener-1.address
+# (Valfritt) logga vad som blev satt
+asadmin get server.network-config.network-listeners.network-listener.http-listener-1.port || true
+asadmin get server.network-config.network-listeners.network-listener.http-listener-1.address || true
 
 # Starta om i foreground
 asadmin stop-domain domain1
