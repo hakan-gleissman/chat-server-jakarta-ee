6ceaf9cc4fdf1b8f55c742fa5c91c5e2affe23bc
hakan-gleissman <hakan.gleissman@gmail.com>
2026-03-02 00:14:35 +0100

    initial

--- start.sh
+++ start.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env sh
 set -e
 
-PORT="${PORT:-8080}"
+PORT="${PORT:?PORT env saknas}"
 echo "Render PORT=${PORT}"
 
 DOMAIN_XML="/opt/glassfish7/glassfish/domains/domain1/config/domain.xml"
 
-# Byt port på vanliga http listeners (8080/8181 -> $PORT)
-# (vissa GF-domäner använder http-listener-2 som faktiskt lyssnar på 8181)
-sed -i "s/network-listener name=\"http-listener-1\" port=\"[0-9]\+\"/network-listener name=\"http-listener-1\" port=\"${PORT}\"/g" "$DOMAIN_XML" || true
-sed -i "s/network-listener name=\"http-listener-2\" port=\"[0-9]\+\"/network-listener name=\"http-listener-2\" port=\"${PORT}\"/g" "$DOMAIN_XML" || true
+# Sätt http-listener-1 till Render-port
+sed -i "s/network-listener name=\"http-listener-1\" port=\"[0-9]\+\"/network-listener name=\"http-listener-1\" port=\"${PORT}\"/g" "$DOMAIN_XML"
 
-# Försök sätta address=0.0.0.0 (om address-attribut inte finns så händer inget)
+# Bind till 0.0.0.0 (om address-attribut saknas blir det ingen ändring, men ok)
 sed -i "s/network-listener name=\"http-listener-1\" /network-listener name=\"http-listener-1\" address=\"0.0.0.0\" /" "$DOMAIN_XML" || true
-sed -i "s/network-listener name=\"http-listener-2\" /network-listener name=\"http-listener-2\" address=\"0.0.0.0\" /" "$DOMAIN_XML" || true
 
-# Starta direkt i foreground (ingen extra start/stop)
+# Stäng andra listeners som annars kan ställa till det
+sed -i "s/network-listener name=\"http-listener-2\" enabled=\"true\"/network-listener name=\"http-listener-2\" enabled=\"false\"/g" "$DOMAIN_XML" || true
+sed -i "s/network-listener name=\"admin-listener\" enabled=\"true\"/network-listener name=\"admin-listener\" enabled=\"false\"/g" "$DOMAIN_XML" || true
+
 exec asadmin start-domain -v domain1
