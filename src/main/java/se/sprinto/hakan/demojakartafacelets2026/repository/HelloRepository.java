187e5468f0fe9e1cae05ac4ea0a8bb194839cee9
hakan-gleissman <hakan.gleissman@gmail.com>
2026-03-01 23:13:38 +0100

    initial

--- src/main/java/se/sprinto/hakan/demojakartafacelets2026/repository/HelloRepository.java
+++ src/main/java/se/sprinto/hakan/demojakartafacelets2026/repository/HelloRepository.java
@@ -1,21 +1,23 @@
 package se.sprinto.hakan.demojakartafacelets2026.repository;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import se.sprinto.hakan.demojakartafacelets2026.model.HelloMessage;
 
 import java.util.List;
 
 @ApplicationScoped
 public class HelloRepository {
-    @PersistenceContext(unitName = "jakarta-ee-demo")
-    private EntityManager entityManager;
+//    @PersistenceContext(unitName = "jakarta-ee-demo")
+//    private EntityManager entityManager;
 
     public List<HelloMessage> getMessages() {
-        return entityManager
-                .createQuery("SELECT m FROM HelloMessage m", HelloMessage.class)
-                .getResultList();
+//        return entityManager
+//                .createQuery("SELECT m FROM HelloMessage m", HelloMessage.class)
+//                .getResultList();
+        HelloMessage message = new HelloMessage();
+        message.setMessage("Hello world");
+        message.setId(1l);
+        return List.of(message);
     }
 
 }
